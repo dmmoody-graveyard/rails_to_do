4a4539427b5ff408c3e31c1feb55cff1e3bfdef8
Duane M. Moody <dmmoody@gmail.com>
2015-03-13 13:39:22 -0700

    add new, save, update and delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,42 @@ class ListsController < ApplicationController
     render :index
   end
 
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   def show
     @list = List.find(params[:id])
     render :show
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
   end
 end
